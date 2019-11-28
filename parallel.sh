b05724983b35965da97733ffd6c2c8704c08fc69
Ria (Luna) <woodstock@egoat.ch>
2019-11-28 16:05:31 +0100

    no optimization option O3, compiler optimizes out repeats

--- parallel.sh
+++ parallel.sh
@@ -5,11 +5,11 @@
 #gcc -O3 -o parallel_OpenMP parallel_OpenMP.c -lm
 #./parallel_non
 
-echo -e 'Threads\tCPU time\tError'
+echo 'Threads\tCPU time\tError'
 
 # serial
 echo '  Serial'
-gcc -O3 -o parallel_non parallel_non.c -lm
+gcc -o parallel_non parallel_non.c -lm
 ./parallel_non
 
 # openMP
@@ -19,7 +19,7 @@ OMP_THREADS=1
 OMP_THREADS_MAX=12
 while [ $OMP_THREADS -le $OMP_THREADS_MAX ]
 do
-    gcc -O3 -fopenmp -o parallel_OpenMP parallel_OpenMP.c -lm -DOMP_THREADS=${OMP_THREADS}
+    gcc -fopenmp -o parallel_OpenMP parallel_OpenMP.c -lm -DOMP_THREADS=${OMP_THREADS}
     set OMP_NUM_THREADS=${OMP_THREADS}
     ./parallel_OpenMP
     OMP_THREADS=`expr $OMP_THREADS + 1`
@@ -27,8 +27,8 @@ done
 
 # MPI
 echo '  MPI'
-mpicc -o parallel_MPI parallel_MPI.c
-mpirun -np 4 ./parallel_MPI
+mpicc.mpich -o parallel_MPI parallel_MPI.c
+mpirun.mpich -np 4 ./parallel_MPI
 
 # MP + openMP
 
